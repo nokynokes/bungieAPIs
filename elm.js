542ab6ca9198a1539a45b129e3b6a81fdd2ceb05
nokynokes <nokynokes@gmail.com>
2021-12-27 17:30:47 -0700

    re adding api key

--- elm.js
+++ elm.js
@@ -7986,6 +7986,7 @@ var $author$project$Main$getAccessToken = F4(
 				{
 					headers: _List_fromArray(
 						[
+							A2($elm$http$Http$header, 'X-API-Key', '21fb29f290494bf3af8e86ed46a8d98e'),
 							A2($elm$http$Http$header, 'Access-Control-Allow-Origin', '*')
 						])
 				}));
